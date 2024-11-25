199b86a23ac3c5b086472363d4be0d4f12fa4247
Fulltvtek <166868431+Fulltvtek@users.noreply.github.com>
2024-11-25 19:01:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function switchChannel(channelSrc) {
   const player = document.getElementById("player");
   player.src = channelSrc;
-  player.play();
 
   // Atualiza informações do programa (mock)
   const title = document.getElementById("program-title");
   const description = document.getElementById("program-description");
 
   switch (channelSrc) {
-    case 'channel1.mp4':
-      title.innerText = "Jornal Local";
-      description.innerText = "Notícias e atualidades ao vivo.";
+    case 'https://reidoscanais.tv/embed/?id=globorj-globorio':
+      title.innerText = "Globo RJ";
+      description.innerText = "Transmissão ao vivo da Globo RJ.";
       break;
-    case 'channel2.mp4':
-      title.innerText = "Sessão de Filmes";
-      description.innerText = "Filmes em alta rotação.";
+    case 'https://reidoscanais.tv/embed/?id=recordrj-recordrio':
+      title.innerText = "Record RJ";
+      description.innerText = "Transmissão ao vivo da Record RJ.";
       break;
-    case 'channel3.mp4':
-      title.innerText = "Show ao Vivo";
-      description.innerText = "Música e entretenimento em tempo real.";
+    case 'https://reidoscanais.tv/embed/?id=sbt-sbtrio':
+      title.innerText = "SBT RJ";
+      description.innerText = "Transmissão ao vivo do SBT RJ.";
       break;
     default:
       title.innerText = "Canal Desconhecido";
